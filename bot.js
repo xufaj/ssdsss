4a9ce4cd4d04f82bea1d420af5dc188c90ce05ed
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-09 20:36:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,19 +18,35 @@ client.on("ready", () => {
 });
 
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
+const prefix = "Your Prefix";
+const token = "Token Your Bot";
+
+client.on("ready", () => {
+  console.log("power ticket | Logged in! Server count: ${client.guilds.size}");
+  client.user.setGame(`*help`);
+});
+
+
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   if (message.content.toLowerCase().startsWith(prefix + `help`)) {
     const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: power Tickety
- Help`)
+    .setTitle(`:mailbox_with_mail: Vulnix Help`)
     .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm power Tickety
-, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .setDescription(`Hello! I'm power ticket, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
     .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about power Tickety
-`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about power ticket`)
     message.channel.send({ embed: embed });
   }
 
@@ -89,5 +105,4 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 }
 
 });
-
 client.login(process.env.BOT_TOKEN);
