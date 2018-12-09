69a668472d6e7ce6dbeb9fa0790fe4f06a2eb7a5
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-09 20:40:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,5 +84,15 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
     });
 }
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`power ticket - Script By : SAJED`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : SAJED ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`*help `,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
 });
+ 
 client.login(process.env.BOT_TOKEN);
